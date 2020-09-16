393d128161d4d7d0f118e6241c6459107da8c51b
Rahmat Mozafari <38719925+Mozafari1@users.noreply.github.com>
2020-09-16 10:03:45 +0200

    block di

--- odd_even_sort_hw/odd_even_sort_hw.srcs/sources_1/new/top_odd_even_sort.vhd
+++ odd_even_sort_hw/odd_even_sort_hw.srcs/sources_1/new/top_odd_even_sort.vhd
@@ -12,7 +12,7 @@ entity top_odd_even_sort is
     SORT_NETWORK_SIZE: integer := 5
   );
   Port (
-    clk, rst, clear, enable: in std_logic;
+    clk,clear, enable: in std_logic;
     data_in: in layer_signals_t(0 to (SIZE - 1));
     data_out: out layer_signals_t(0 to (SIZE - 1));
     sort_done: out std_logic
@@ -24,20 +24,10 @@ architecture arch of top_odd_even_sort is
     type sort_network_signals_t is array (0 to (SORT_NETWORK_SIZE - 2)) of layer_signals_t (0 to SIZE - 1);
     --
     signal sort_network_intermediate: sort_network_signals_t;
-    
-
+    signal  sort_network_output : layer_signals_t (0 to SIZE - 1);
+    signal mux_out, reg_in, reg_out: layer_signals_t (0 to SIZE - 1);
+    signal sorted, reg_mux, load:std_logic;
 begin
-
-
-
---process(clk,rst)
---begin
---    if(rst = '1') then
-        
---    else if (rising_edge(clk)) then
-       
---    end if;
---end process;    
 
 sort_network: for i in 0 to (SORT_NETWORK_SIZE - 1) generate
 
@@ -47,12 +37,60 @@ sort_network: for i in 0 to (SORT_NETWORK_SIZE - 1) generate
             SIZE => SIZE
         )
         port map(   
-            data_in => data_in,
-            data_out =>  sort_network_intermediate(i)
+           data_in => data_in,
+           data_out =>  sort_network_intermediate(i),
+           sorted => sorted
         );
     end generate;
-    
-  
+
+      sort_layer: if (i > 0 and i < (SORT_NETWORK_SIZE -2)) generate
+      layer: entity work.sort_layer
+        generic map(
+            SIZE => SIZE
+        )
+        port map(   
+            data_in=> sort_network_intermediate(i-1),
+            data_out =>  sort_network_intermediate(i),
+            sorted => sorted
+        );
+     
+    end generate;
+  last_layer: if (i = (SORT_NETWORK_SIZE -2)) generate
+      layer: entity work.sort_layer
+        generic map(
+            SIZE => SIZE
+        )
+        port map(   
+           data_in => sort_network_intermediate(i-1),
+           data_out =>  sort_network_output,
+           sorted => sorted
+        );
+    end generate;
 end generate;
+-- iterative sort network
+
+
+      iterative_sort_layer: entity work.sort_layer
+        generic map(
+            SIZE => SIZE
+        )
+        port map(   
+           data_in => mux_out,
+           data_out =>  reg_in,
+           sorted => sorted
+           
+        );
+    
+    mux_out <=reg_out when reg_mux ='1' else  sort_network_output;
+ 
+
+ data_out <=    reg_out;
+ process (clk)
+ begin 
+  if(rising_edge (clk) and load ='1') then
+    reg_out <= reg_in;
+  end if;
+end process;
+ 
 
 end arch;
